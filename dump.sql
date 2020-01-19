cefffa569d0b64f9d8157e85d02fa99b52e57666
Roman Pukhliy <pukhliyroman@gmail.com>
2020-01-19 18:03:18 +0200

    SQL dump definition

---
+++ dump.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `schools` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `board` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `school_students` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `board` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `school_id` (`school_id`),
+  CONSTRAINT `school_students_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `school_student_grades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_id` int(11) NOT NULL,
+  `school_student_id` int(11) NOT NULL,
+  `grade` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `school_id` (`school_id`),
+  KEY `school_student_id` (`school_student_id`),
+  CONSTRAINT `school_student_grades_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`),
+  CONSTRAINT `school_student_grades_ibfk_2` FOREIGN KEY (`school_student_id`) REFERENCES `school_students` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
